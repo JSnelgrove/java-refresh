2260435ebbe41ea9ed21ca214168fed0a8607f06
Jack Snelgrove <jack.snelgrove@me.com>
2025-02-21 00:23:17 -0500

    Created blank Maven project

---
+++ JavaRefresh/src/main/java/com/refresh/java/model/Book.java
@@ -0,0 +1,63 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+
+@Entity
+public class Book{
+    
+    int id;
+    String title;
+    String author;
+    int isbn;
+    Date publishedDate;
+
+    public Book(){}
+
+    public Book(int id, String title,String author, int isbn, Date publishedDate){
+        this.id= id;
+        this.title = title;
+        this.author= author;
+        this.isbn= isbn;
+        this.publishedDate= publishedDate;
+    }   
+
+    public int getID(){
+        return this.id;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public int getISBN(){
+        return this.isbn;
+    }
+
+    public void setISBN(int isbn){
+        this.isbn = isbn;
+    }   
+    
+    public String toString(){
+        return  "Book [id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", publishedDate=" + publishedDate + "]";
+    }
+}
